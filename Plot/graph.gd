2620d127dd1c1b6e8e944392715d74dac526a076
TurboSeb <turbosebb@gmail.com>
2024-12-24 16:29:19 +0100

    scale from mouse position, axis specific scaling

--- Plot/graph.gd
+++ Plot/graph.gd
@@ -36,18 +36,24 @@ var scale_limit: Vector2
 
 ## Connected to the graph_container resized() signal, called a little after ready on startup
 func graph_container_resized() -> void:
+	if panning:
+		return
 	graph_container_size = graph_container.size
 	set_panning_limit()
 	
-	var axis_scale_limit =  max(graph_container_size.x / size.x, graph_container_size.y / size.x)
-	print(axis_scale_limit)
-	scale_limit = Vector2(axis_scale_limit, axis_scale_limit)
+	var scale_limit_x =  graph_container_size.x / size.x
+	var scale_limit_y =  graph_container_size.y / size.y
+	scale_limit = Vector2(scale_limit_x, scale_limit_y)
+	print(scale_limit)
 
 ## called on zoom
-func clamp_position() ->void:
+func clamp_position(new_position_x: float = , new_position_y: float = ) ->void:
 	set_panning_limit()
-	position.x = clamp(position.x, panning_limit.x, 0)
-	position.y = clamp(position.y, panning_limit.y, 0)
+	if new_position_x:
+		position.x = clamp(new_position_x, panning_limit.x, 0)
+	if new_position_y:
+		position.y = clamp(new_position_y, panning_limit.y, 0)
+	
 	
 
 func set_panning_limit() ->void:
@@ -61,7 +67,7 @@ func _ready() -> void:
 
 func _draw() -> void:
 	draw_polyline(chart, line_color, line_width)
-
+	
 ## Called when the input is done as the graph is moused over, also works when releasing outside of the graph
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -71,25 +77,90 @@ func _on_gui_input(event: InputEvent) -> void:
 		if Input.is_action_just_released("panning"):
 			deactivate_panning()
 			print("no panning")
+		if Input.is_action_just_released("y_zoom_in"):
+			if !panning:
+				zoom_in_y()
+				return
+		if Input.is_action_just_released("y_zoom_out"):
+			if !panning:
+				zoom_out_y()
+				return
+		
+		if Input.is_action_just_released("x_zoom_in"):
+			if !panning:
+				zoom_in_x()
+				return
+		if Input.is_action_just_released("x_zoom_out"):
+			if !panning:
+				zoom_out_x()
+				return
+		
+		
 		if Input.is_action_just_released("zoom_in"):
 			if !panning:
-				scale = scale + zoom_step_vector
-				clamp_position()
-				graduation.recalculate()
+				zoom_in_x()
+				zoom_in_y()
+				#var new_scale = scale + zoom_step_vector
+				#var scale_ratio = Vector2(new_scale.x/scale.x - 1, new_scale.y/scale.y - 1)
+				#
+				#var new_position = position + (global_position - get_viewport().get_mouse_position()) * scale_ratio
+				#scale = new_scale
+				#clamp_position(new_position.x, new_position.y)
+				#graduation.recalculate()
+				
 		if Input.is_action_just_released("zoom_out"):
 			if !panning:
-				var new_scale =  max(scale.x - zoom_step, scale_limit.x)
-				scale = Vector2(new_scale, new_scale)
-				clamp_position()
-				graduation.recalculate()
+				zoom_out_x()
+				zoom_out_y()
+				#var new_scale =  Vector2(max(scale.x - zoom_step, scale_limit.x), max(scale.y - zoom_step, scale_limit.y))
+				#var scale_ratio = new_scale/scale - Vector2(1, 1)
+				#var new_position = position + (global_position - get_viewport().get_mouse_position()) * scale_ratio
+				#scale = new_scale
+				#clamp_position(new_position.x, new_position.y)
+				#graduation.recalculate()
 
+func zoom_in_x():
+	var new_scale_x = scale.x + zoom_step
+	var scale_ratio_x = new_scale_x/scale.x - 1
+				
+	var new_position_x = position.x + (global_position.x - get_viewport().get_mouse_position().x) * scale_ratio_x
+	scale.x = new_scale_x
+	clamp_position(new_position_x, position.y)
+	graduation.recalculate()
+
+func zoom_out_x():
+	var new_scale_x = max(scale.x - zoom_step, scale_limit.x)
+	var scale_ratio_x = new_scale_x/scale.x - 1
+				
+	var new_position_x = position.x + (global_position.x - get_viewport().get_mouse_position().x) * scale_ratio_x
+	scale.x = new_scale_x
+	clamp_position(new_position_x, position.y)
+	graduation.recalculate()
+
+func zoom_in_y():
+	var new_scale_y = scale.y + zoom_step
+	var scale_ratio_y = new_scale_y/scale.y - 1
+				
+	var new_position_y = position.y + (global_position.y - get_viewport().get_mouse_position().y) * scale_ratio_y
+	scale.y = new_scale_y
+	clamp_position(position.x, new_position_y)
+	graduation.recalculate()
+
+func zoom_out_y():
+	var new_scale_y = max(scale.y - zoom_step, scale_limit.y)
+	var scale_ratio_y = new_scale_y/scale.y - 1
+				
+	var new_position_y = position.y + (global_position.y - get_viewport().get_mouse_position().y) * scale_ratio_y
+	scale.y = new_scale_y
+	clamp_position(position.x, new_position_y)
+	graduation.recalculate()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if panning:
 			var new_position = position + event.relative
-			position.x = clamp(new_position.x, panning_limit.x, 0)
-			position.y = clamp(new_position.y, panning_limit.y, 0)
+			clamp_position(new_position.x, new_position.y)
+			$"../Label".text = "X: " + str(position.x) + "\nY: " + str(position.y)
 			graduation.recalculate()
 
 
